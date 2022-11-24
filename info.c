1ebd9f800ea04867ebcd7e5fb9ae11485636ed10
oroz28 <117312149+oroz28@users.noreply.github.com>
2022-11-24 15:38:08 +0100

    Delete info.c

--- info.c
+++
@@ -1,8 +0,0 @@
-#include "info.h"
-#include <float.h>
-
-//Inicializar todo al maximo o minimo posible
-void inicializarInfo(struct info *info) {
-	info->minAge = info->minDebt = info->minYearsEmployed = info->minCreditScore = info->minIncome = DBL_MAX;	//DBL MAX es el maximo valor para un double
-	info->maxAge = info->maxDebt = info->maxYearsEmployed = info->maxCreditScore = info->maxIncome = DBL_MIN;	//Lo mismo para el MIN
-}
